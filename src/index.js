aeaccbe496cb9cfb0009f0e2fe87611e01377f7b
Dalianaguirre <danathap@gmail.com>
2023-08-03 19:37:47 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import axios from 'axios'
 
-//axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://countriesback-production-74ac.up.railway.app/';
+//axios.defaults.baseURL = 'http://localhost:3001';      //para trabajar de manera local
+axios.defaults.baseURL = 'https://countriesback-production-74ac.up.railway.app/';     //cuando queramos debugear o actualizar nuestro deploy del front. se conecta con el deploy del servidor deployado
 
 ReactDOM.render(
   <Provider store={store}>
